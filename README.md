1641edff5bde0e6028b465ee76e93b18abcbecd7
banebo <branislavbozejac@gmail.com>
2019-03-09 12:13:27 +0100

    message

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # linux
+
+Linux setup. Installes needed packages, configures vim & i3, sets fish as the
+deffault shell, adds some scripts to $HOME/.bin and some fish functions
--- setup.sh
+++ setup.sh
@@ -1,142 +1,51 @@
 #!/usr/bin/env bash
 
-############
-##INSTALLS##
-############
-# vim
-# qbittorrent
-# fish
-# i3
-# mysql (mariadb)
-# mysql-workbench
-# chromium
+###############
+# LINUX SETUP #
+###############
 
+# install required packages
+echo "[+] Starting install..."
+sudo pacman -S $( < requirements.txt )
 
-type pacman > /dev/null
-if [[ $? -eq 0 ]]; then
-    install="sudo pacman -S "
-else
-    echo "[-] No pacman, no installs..."
-    exit 1
+# add $HOME/.bin to $PATH
+if [[ ! -d $HOME/.bin ]]; then
+    mkdir $HOME/.bin
 fi
+echo "export PATH=$PATH:$HOME/.bin" >> $HOME/.profile
 
-type vim > /dev/null
-if [[ $? -ne 0 ]]; then
-    $install "vim"
-    echo "[+] Vim installed"
-fi
-
-type qbittorrent > /dev/null
-if [[ $? -ne 0 ]]; then
-    $install "qbittorrent"
-    echo "[+] Qbittorrent installed"
-fi
-
-type fish > /dev/null
-if [[ $? -ne 0 ]]; then
-    $install "fish"
-    echo "[+] Fish installed"
-fi
-
-read -p "[?] Set fish as default for $USER [y/n]: " choice
-if [[ $choice == "y" ]]; then
-    sudo usermod -s /usr/bin/fish $USER
-fi
-
-type i3 i3status > /dev/null
-if [[ $? -ne 0 ]]; then
-    $install "i3-wm"
-    $install "i3status"
-    $install "dmenu-manjaro"
-    $install "i3lock"
-    echo "[+] i3 installed"
-fi
-
-type mysql > /dev/null
-if [[ $? -ne 0 ]]; then
-    echo
-    read -p "[?] Install mysql [y/n]: " choice
-    if [[ $choice == 'y' ]]; then
-        $install "mariadb"
-        echo "[+] Mariadb (mysql) installed"
-    fi
-fi
-
-type mysql-workbench > /dev/null
-if [[ $? -ne 0 ]]; then
-    echo
-    read -p "[?] Install mysql-workbench [y/n]: " choice
-    if [[ $choice == 'y' ]]; then
-        $install "mysql-workbench"
-        echo "[+] Mysql-workbench installed"
-    fi
-fi
-
-type chromium > /dev/null
-if [[ $? -ne 0 ]]; then
-    read -p "[?] Install chromium [y/n]: " choice
-    if [[ $choice == 'y' ]]; then
-        $install "chromium"
-        echo "[+] Chromium installed"
-    fi
-fi
-
-type firefox > /dev/null
-if [[ $? -eq 0 ]]; then
-    read -p "[?] Remove firefox [y/n]: " choice
-    if [[ $choice == 'y' ]]; then
-        sudo pacman -Rs firefox
-        echo "[+] Firefox removed"
-    fi
-fi
-
-type java javac > /dev/null
-if [[ $? -ne 0 ]]; then
-    echo "[?] Enter java version to install: "
-    printf "\t[%d] %s\n" 1 "java8" 2 "java10" 3 "Exit"
-    read -p "[?] Enter number of choice: " choice
-    if [[ $choice -eq 1 ]]; then
-        $install "jdk8-openjdk"
-        echo "[+] Java8 installed "
-    elif [[ $choice -eq 2 ]]; then
-        $install "jdk10-openjdk"
-        echo "[+] Java10 installed"
-    fi
-fi
-
-read -p "[?] Install javaFX [y/n]: " choice
+# setup vim
+printf "\n[?] Setup vim? [y/n]: "
+read choice
 if [[ $choice == 'y' ]]; then
-    $install "java-openjfx"
+    git clone -q https://github.com/banebo/vim $HOME/vim
+    ./$HOME/vim/setup
+    rm -rf $HOME/vim
 fi
+printf "\t[+] Done.\n"
 
-echo
-read -p "[?] Setup vim [y/n]: " choice
+# setup i3
+printf "[?] Setup i3? [y/n]: "
+read $choice
 if [[ $choice == 'y' ]]; then
-    git clone https://github.com/banebo/vim $HOME/vim
-    bash $HOME/vim/setup.sh
-    if [[ $? -eq 0 ]]; then
-        rm -rf $HOME/vim
-        printf "\t[+] Done\n"
-    fi
+    git clone -q https://github.com/banebo/i3 $HOME/i3
+    ./$HOME/i3/setup
+    rm -rf $HOME/i3
 fi
+printf "\t[+] Done."
 
-echo
-read -p "[?] Setup i3 [y/n]: " choice
+# set fish as deffault shell
+read -p "[?] Set fish as deffault shell? [y/n]: " $choice
 if [[ $choice == 'y' ]]; then
-    git clone https://github.com/banebo/i3 $HOME/i3
-    cd $HOME/i3/
-    bash setup.sh
-    cd ..
-    rm -rf i3
-    echo "    [+] Done"
+    usermod -s /usr/bin/fish $USER
 fi
+printf "\t[+] Done."
 
-type libinput >> /dev/null
-if [[ $? -ne 0 ]]; then
-    $install "libinput"
-fi
+# copy scripts -> $HOME/.bin
+cp -r scripts/* $HOME/.bin
 
-type xinput >> /dev/null
-if [[ $? -ne 0 ]]; then
-    $install "xorg-xinput"
-fi
+# copy fish functions
+cp -r fish_functions/* $HOME/.config/fish/functions/
+
+printf "[+] All done."
+exit 0
