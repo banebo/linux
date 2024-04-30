f0eefde1a9402398ed9d58ad8610f2a587f398cf
banebo <branislavbozejac@gmail.com>
2024-04-30 07:40:31 -0400

    apt source link fix

--- post-install/pve.sh
+++ post-install/pve.sh
@@ -215,7 +215,7 @@ deb https://deb.debian.org/debian bookworm-updates main contrib
 
 # Proxmox VE pve-no-subscription repository provided by proxmox.com,
 # NOT recommended for production use
-deb https://download.proxmox.com/debian/pve bookworm pve-no-subscription
+deb http://download.proxmox.com/debian/pve bookworm pve-no-subscription
 
 # security updates
 deb https://security.debian.org/debian-security bookworm-security main contrib
